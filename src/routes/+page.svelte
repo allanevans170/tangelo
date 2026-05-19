f0fa47f9bd448024e5ee0841131e54bf3af78a89
allanevans170 <allanevans170@gmail.com>
2026-05-19 17:32:57 -0600

    yeet cloudflare pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <title>tangelo productions</title>
 
 <h1>tangelo productions llc</h1>
-<p>Visit <a href="this website belongs to Allan Evans, owner and CEO of tangelo productions llc</p>
+<p>this website belongs to Allan Evans, owner and CEO of tangelo productions llc</p>
