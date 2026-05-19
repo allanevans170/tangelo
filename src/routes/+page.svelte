38b77189132fae40125609ff94edbc125276fb3b
allanevans170 <allanevans170@gmail.com>
2026-05-19 17:17:26 -0600

    initialize main page with personalized text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<title>tangelo productions</title>
+
+<h1>tangelo productions llc</h1>
+<p>Visit <a href="this website belongs to Allan Evans, owner and CEO of tangelo productions llc</p>
